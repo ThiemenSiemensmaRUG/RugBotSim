126acb1bfb63c5338fe402ffdc234c7fd6f0fbbf
diana-catana <dia.catana@gmail.com>
2024-11-06 16:43:40 -0500

    Successfully read from file

--- controllers/inspection_controller/Algorithm_Template.hh
+++ controllers/inspection_controller/Algorithm_Template.hh
@@ -18,11 +18,6 @@ typedef std::complex<double> Complex;
 typedef std::vector<Complex> CArray;
 typedef std::vector<double> Array;
 
-
-
-
-
-
 class Algorithm1 {
 public:
     enum AlgoStates {
@@ -31,14 +26,12 @@ public:
         STATE_PAUSE // etc....
     };
 
-
     AlgoStates states = STATE_RW;
-
 
     // Time step for the simulation
     enum { TIME_STEP = 20 };
 
-    Algorithm1() : settings(),robot(TIME_STEP),environ("world.txt"),radio(robot.d_robot,TIME_STEP) {};
+    Algorithm1() : settings(), robot(TIME_STEP), environ("world.txt"), radio(robot.d_robot, TIME_STEP) {};
 
     void run();
     void recvSample();
@@ -52,89 +45,77 @@ private:
     std::vector<int> pos;
     int sample_ = 0;
 
+    void readDataFromFile(const std::string& filename);
 };
 
 void Algorithm1::run() {
     std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
-   
+
     robot.setCustomData("");
     settings.readSettings();
 
-    while(robot.d_robot->step(TIME_STEP) != -1) {
-        
-        switch(states) {
-
+    while (robot.d_robot->step(TIME_STEP) != -1) {
+        switch (states) {
             case STATE_RW:
-
-                if(robot.RandomWalk()==1){
+                if (robot.RandomWalk() == 1) {
                     states = STATE_OBS;
                 }
-
                 break;
 
             case STATE_OBS:
                 sample_ = environ.getSample(robot.getPos()[0], robot.getPos()[1]);
-                std::cout<<sample_<<"\n";
-                
+                std::cout << "Sample: " << sample_ << " Robot position: " 
+                          << 1.0 * robot.getPos()[0] / 100 << " " << 1.0 * robot.getPos()[1] / 100 << "\n";
 
-                std::cout<<"Sample: "<<sample_<<" Robot position: "<< 1.0 * robot.getPos()[0] / 100<<" "<< 1.0 * robot.getPos()[1] / 100<<"\n";
-                // ADD INFERENCE:
-                // load CNN model
-                // read data: type A if sampled 0, type B if sampled 1
-                // use model to classify vibration
-                // return vibration type
+                if (sample_ == 0) {
+                    // Read data from file
+                    readDataFromFile("../../data/capture1_60hz_30vol.txt");  
+                }
 
-                states = STATE_RW;    
-
-
-
+                states = STATE_RW;
                 break;
-
 
             case STATE_PAUSE:
                 // Pause logic here
                 break;
         }
-        if(robot.d_robot->getTime()>5){
-        robot.setCustomData( std::to_string((int) robot.d_robot->getTime())+
-        ","+std::string(robot.d_robot->getName().substr(1,1)) );
+        
+        if (robot.d_robot->getTime() > 5) {
+            robot.setCustomData(std::to_string((int)robot.d_robot->getTime()) +
+                "," + std::string(robot.d_robot->getName().substr(1, 1)));
         }
     }
 }
 
+void Algorithm1::readDataFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file " << filename << std::endl;
+        return;
+    }
 
-void Algorithm1::recvSample(){
+    std::string line;
+    while (std::getline(file, line)) {
+        // Process each line here as needed
+        std::cout << "Read line: " << line << std::endl;
+
+        // Example: parse line data (if numeric data is stored in lines)
+        // double data = std::stod(line); // If you need to convert line to a number
+    }
+    file.close();
+}
+
+void Algorithm1::recvSample() {
     std::vector<int> messages = radio.getMessages();
+    for (int sample : messages) {
         // Process received messages
-        for (int sample : messages) {
-            //Do something
-        }
+    }
 }
 
-void Algorithm1::sendSample(int sample){
-
-    // Determine the message to send based on decision flag or observation color
+void Algorithm1::sendSample(int sample) {
     int const *message;
-
     message = &sample;
-    // Send the message
     radio.sendMessage(message, sizeof(message));
-
 }
-
 
 #endif // INCLUDED_ALGORITHM1_HH_
-
-
-
-
-
-
-
-
-
-
-
-
-
-
