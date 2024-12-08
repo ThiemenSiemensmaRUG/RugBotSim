e7732b4ac4a4aecb8e94abba584bef53468a607a
Niels <46285502+TheAefka@users.noreply.github.com>
2024-12-09 00:24:10 +0100

    Applied Google checkstyle reformat, fixed comparison of different fields

--- controllers/inspection_controller/Algorithm1.hh
+++ controllers/inspection_controller/Algorithm1.hh
@@ -1,47 +1,41 @@
 #ifndef INCLUDED_ALGORITHM1_HH_
 #define INCLUDED_ALGORITHM1_HH_
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <complex>
-#include <vector>
 #include <cmath>
-#include <utility>
+#include <complex>
 #include <filesystem>
-#include "filtering.hh"
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
+
 #include "RugBot.hh"
 #include "SHM.hh"
-#include "radio.hh"
 #include "controller_settings.hh"
+#include "filtering.hh"
+#include "radio.hh"
 
 typedef std::complex<double> Complex;
 typedef std::vector<Complex> CArray;
 typedef std::vector<double> Array;
 
-
-
-
-
-
 class Algorithm1 {
-public:
+   public:
     enum AlgoStates {
-        STATE_RW,  // State: Random Walk
-        STATE_OBS, // State: Observe Color
-        STATE_PAUSE // etc....
+        STATE_RW,    // State: Random Walk
+        STATE_OBS,   // State: Observe Color
+        STATE_PAUSE  // etc....
     };
 
-
     AlgoStates states = STATE_RW;
-
-
-    
 
     // Time step for the simulation
     enum { TIME_STEP = 20 };
 
-    Algorithm1() : settings(),robot(TIME_STEP),naturalFreq(2.0,2.0,100.0),radio(robot.d_robot,TIME_STEP) {};
-
+    Algorithm1()
+        : settings(),
+          robot(TIME_STEP),
+          naturalFreq(2.0, 2.0, 100.0),
+          radio(robot.d_robot, TIME_STEP) {};
     void run();
     void getSample(int posx, int posy);
     void detectPeaks(Array freqArray);
@@ -49,24 +43,23 @@ public:
     void recvSample();
     void sendSample(int sample);
 
-private:
+   private:
     ControllerSettings settings;
     RugRobot robot;
     eigenFreq naturalFreq;
     Radio_Rover radio;
     std::vector<int> pos;
 
- 
-    double sample_freq = 200.0; //Sample rate in Hz
-    double sample_length = 4.5; //Sample length in seconds
+    double sample_freq = 200.0;  // Sample rate in Hz
+    double sample_length = 4.5;  // Sample length in seconds
     double sample_pause = 0.0;
-    double dt = 1/sample_freq; //Time between samples
+    double dt = 1.0 / sample_freq;  // Time between samples
 
     double cutoff_freq = 1.0;  // Cutoff frequency in Hz
-    std::vector<double> b = { 0.04125354, 0.08250707, 0.04125354 };
-    std::vector<double> a = {  1.      ,   -1.34896775 , 0.51398189 };
+    std::vector<double> b = {0.04125354, 0.08250707, 0.04125354};
+    std::vector<double> a = {1., -1.34896775, 0.51398189};
 
-    int N_samples = ((sample_freq) * sample_length);
+    int N_samples = ((sample_freq)*sample_length);
     int half_size = N_samples / 2 + 1;
 
     std::vector<double> numbers;
@@ -75,8 +68,6 @@ private:
     std::vector<double> pos_abs_fft_results;
     std::vector<double> peak_freq;
     std::vector<double> peak_mag;
-
-
 };
 
 /**
@@ -100,7 +91,7 @@ private:
  * The function also outputs the robot's time, name, estimated frequency, position, alpha, beta, and iteration count.
  */
 void Algorithm1::run() {
-    //std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+    // std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
     pos = roundToNearest10(robot.getPos());
     robot.setCustomData("");
     settings.readSettings();
@@ -109,39 +100,35 @@ void Algorithm1::run() {
         naturalFreq.a[i] = (double)settings.values[i];
     }
     for (int i = 0; i <= 1; ++i) {
-        naturalFreq.b[i] = (double)settings.values[i+2];
+        naturalFreq.b[i] = (double)settings.values[i + 2];
     }
     naturalFreq.learning_rate = settings.values[4];
     naturalFreq.upper_freq = settings.values[5];
-    
-    CArray fft_results(N_samples); //complex array to store FFT results
+
+    CArray fft_results(N_samples);  // complex array to store FFT results
     Array freqArray = createFrequencyArray(sample_freq, N_samples);
 
-    
-
-    while(robot.d_robot->step(TIME_STEP) != -1) {
-        
-        switch(states) {
-
+    while (robot.d_robot->step(TIME_STEP) != -1) {
+        switch (states) {
             case STATE_RW:
 
-                if(robot.RandomWalk()==1){
- 
+                if (robot.RandomWalk() == 1) {
                     states = STATE_OBS;
                 }
-                if (naturalFreq.iteration>0)
-                    {recvSample();}
+                if (naturalFreq.iteration > 0) {
+                    recvSample();
+                }
                 break;
 
             case STATE_OBS:
-                sample_pause+=TIME_STEP;
-                if (sample_pause < (sample_length * 1000)){
+                sample_pause += TIME_STEP;
+                if (sample_pause < (sample_length * 1000)) {
                     break;
                 }
                 sample_pause = 0.0;
                 pos = roundToNearest10(robot.getPos());
 
-                getSample(pos[0],pos[1]);
+                getSample(pos[0], pos[1]);
                 for (size_t i = 0; i < numbers.size(); ++i) {
                     fft_results[i] = Complex(numbers[i], 0);
                 }
@@ -152,26 +139,24 @@ void Algorithm1::run() {
 
                 naturalFreq.update(naturalFreq.checkSample(peak_freq[0]));
 
-                sendSample(naturalFreq.checkSample(peak_freq[0]));  
-                
-                states = STATE_RW;                  
-                break;
+                sendSample(naturalFreq.checkSample(peak_freq[0]));
 
+                states = STATE_RW;
+                break;
 
             case STATE_PAUSE:
                 // Pause logic here
                 break;
         }
-        if(robot.d_robot->getTime()>5){
-        robot.setCustomData( std::to_string((int) robot.d_robot->getTime())+
-        ","+std::string(robot.d_robot->getName().substr(1,1)) + 
-        ","+std::to_string(naturalFreq.getEstimatedFreq()) +
-        ","+std::to_string(robot.getPos()[0])+
-        ","+std::to_string(robot.getPos()[1])+
-        ","+std::to_string(naturalFreq.alpha)+
-        ","+std::to_string(naturalFreq.beta)+
-        "," + std::to_string(naturalFreq.iteration));
-        
+        if (robot.d_robot->getTime() > 5) {
+            robot.setCustomData(std::to_string((int)robot.d_robot->getTime()) +
+                                "," + std::string(robot.d_robot->getName().substr(1, 1)) +
+                                "," + std::to_string(naturalFreq.getEstimatedFreq()) +
+                                "," + std::to_string(robot.getPos()[0]) +
+                                "," + std::to_string(robot.getPos()[1]) +
+                                "," + std::to_string(naturalFreq.alpha) +
+                                "," + std::to_string(naturalFreq.beta) +
+                                "," + std::to_string(naturalFreq.iteration));
         }
     }
 }
@@ -184,11 +169,9 @@ void Algorithm1::run() {
  */
 void Algorithm1::recvSample() {
     std::vector<int> messages = radio.getMessages();
-        // Process received messages
-        for (int sample : messages) {
-            //std::cout <<"recv " << sample<<'\n';
-            naturalFreq.update(sample);
-        }
+    for (int sample : messages) {
+        naturalFreq.update(sample);
+    }
 }
 
 /**
@@ -216,16 +199,11 @@ void Algorithm1::sendSample(int sample) {
  * @param posy The y-coordinate used to construct the file name.
  */
 void Algorithm1::getSample(int posx, int posy) {
-    
-    // Construct the file path based on posx and posy
     std::filesystem::path currentPath = std::filesystem::current_path();
     // Assuming this code is located in the controllers/inspection_controller directory
     std::filesystem::path basePath = currentPath.parent_path().parent_path();
     std::string filePath = (basePath / "measurements" / ("acc_x" + std::to_string(posx) + "_y" + std::to_string(posy) + ".txt")).string();
 
-    //std::cout << "Attempting to open file: " << filePath << std::endl; // Add this line for debugging
-
-    // Open the file
     std::ifstream inputFile(filePath);
     if (!inputFile.is_open()) {
         std::cerr << "Failed to open file: " << filePath << std::endl;
@@ -236,18 +214,16 @@ void Algorithm1::getSample(int posx, int posy) {
     numbers.clear();
     double number;
 
-    // Read up to N_samples lines from the file
     for (int i = 0; i < N_samples; i++) {
         std::string line;
         if (std::getline(inputFile, line)) {
-            // Convert the line to a double and add it to the vector
+            // Convert to a double and add it to the vector
             number = std::stod(line);
-
             numbers.push_back(number);
         } else {
-            break; // Stop reading if fewer than 5 lines are available
+            break;
         }
-        states= STATE_PAUSE;
+        states = STATE_PAUSE;
     }
 }
 
@@ -270,27 +246,29 @@ void Algorithm1::detectPeaks(Array freqArray) {
     std::vector<double> filtered_fft_backward = backwardButterworth(b, a, filtered_fft_forward);
 
     filtered_fft = filtered_fft_backward;
-    //saveFilteredData(filtered_fft,"fft_filtered.txt");
+    // saveFilteredData(filtered_fft,"fft_filtered.txt");
 
     // Detect peaks in the signal
     std::vector<int> index;
-    for (int i = 1; i < filtered_fft.size() - 1; ++i) {
-        if (filtered_fft[i-1] < filtered_fft[i] && filtered_fft[i+1] < filtered_fft[i]) {
+    for (std::vector<double>::size_type i = 1; i < filtered_fft.size() - 1; ++i) {
+        if (filtered_fft[i - 1] < filtered_fft[i] && filtered_fft[i + 1] < filtered_fft[i]) {
             index.push_back(i);
         }
     }
+    
     // Number of peaks wanted
-    int N_peaks = 3;
+    std::vector<int>::size_type N_peaks = 3;
 
     // Sort indices based on filtered_data values
     std::sort(index.begin(), index.end(), [&](int i1, int i2) {
         return filtered_fft[i1] > filtered_fft[i2];
     });
+
     // Get top N peaks (indices)
     std::vector<int> top_indices;
-    const double min_distance = 0.1;  // Adjust this minimum distance as needed
+    const double min_distance = 0.1;  // Minimum distance between peaks to be considered separate peaks
     double last_freq = -1.0;
-    for (int i = 0; i < N_peaks && i < index.size(); ++i) {
+    for (std::vector<int>::size_type i = 0; i < N_peaks && i < index.size(); ++i) {
         int idx = index[i];
         double current_freq = freqArray[idx];
 
@@ -301,13 +279,12 @@ void Algorithm1::detectPeaks(Array freqArray) {
         }
     }
 
-
     peak_mag.clear();
     peak_freq.clear();
 
     std::vector<std::pair<double, double>> peaks;
     for (const auto& idx : top_indices) {
-        peaks.emplace_back( freqArray[idx],filtered_fft[idx]);
+        peaks.emplace_back(freqArray[idx], filtered_fft[idx]);
     }
 
     // Sort peaks based on frequency
@@ -319,11 +296,10 @@ void Algorithm1::detectPeaks(Array freqArray) {
     for (const auto& peak : peaks) {
         peak_mag.push_back(peak.second);
         peak_freq.push_back(peak.first);
-        //std::cout << "Mag: " << peak.second << ", Frequency: " << peak.first << std::endl;
+        // std::cout << "Mag: " << peak.second << ", Frequency: " << peak.first << std::endl;
     }
 
-    //appendValuesToFile(peak_freq, peak_mag);
-
+    // appendValuesToFile(peak_freq, peak_mag);
 }
 
 /**
@@ -348,4 +324,5 @@ void Algorithm1::saveFilteredData(const std::vector<double>& data, const std::st
 
     outputFile.close();
 }
-#endif // INCLUDED_ALGORITHM1_HH_
+
+#endif  // INCLUDED_ALGORITHM1_HH_
