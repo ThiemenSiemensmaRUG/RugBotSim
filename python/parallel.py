a95e1246cbcd8a4ffe6f36511106ec198d1bfc91
ThiemenSiemensmaRUG <133112888+ThiemenSiemensmaRUG@users.noreply.github.com>
2024-12-17 15:28:44 +0100

    Changes for Matei

--- python/parallel.py
+++ python/parallel.py
@@ -28,110 +28,63 @@ def launch_instance(x,reevaluation = 0,run_dir=0,feedback =0,fill_ratio = 0.48,n
     
     world_creation_seed = instance 
     grid_seed = instance + grid_start_seed
-    c_settings = {"gamma0":x[0],"gamma":x[1],"tau":x[2],"thetaC":x[3],"swarmCount":x[4],"feedback":feedback,'eta':eta,"seed":instance+grid_start_seed,"sample_strategy":sample_strategy,"size":gridsize,"Usp":Usp,"P(FP)":P_fp,"P(FN)":P_fn}
+    c_settings = {"gamma0":x[0],"gamma":x[1],"tau":x[2],"thetaC":x[3],"swarmCount":x[4],"feedback":feedback,'eta':2000,"seed":instance+grid_start_seed,"sample_strategy":sample_strategy,"size":gridsize,"Usp":1500,"P(FP)":P_fp,"P(FN)":P_fn}
     s_settings = {"right_dec":right_dec,"fill_ratio":fill_ratio,"offset_f":0.04,"check_interval":10,"autoexit":1,"run_full":run_full}
     settings = {"reevaluation":reevaluation,"word_creation_seed":world_creation_seed,"grid_seed":grid_seed ,"description":desc}
 
     job.job_setup(c_settings=c_settings,s_settings=s_settings,settings=settings,world_creation_seed=world_creation_seed,grid_seed=grid_seed,fill_ratio=fill_ratio,gridsize=gridsize,grid_ = grid)
     job.run_webots_instance(port=1234+instance)
     fitness = job.get_fitness()
+    #Matei: change folders below according to your path
     job.move_results("/home/thiemenrug/Documents/_temp/",f"parallel_{run_dir}/Instance_{instance}")
     job.remove_run_dir()
     del x
     return fitness
 
 
-def launch_batch(batch_size,workers,x_,run_dir,feedback,fill_ratio,robots,grid=None,size = 5,desc="None"):
+def launch_batch(batch_size,workers, x_, run_dir , feedback ,fill_ratio ,robots,grid=None ,size = 5 ,desc="None"):
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as process_executor:  
         futures = [process_executor.submit(launch_instance, x_,i,run_dir,feedback,fill_ratio,robots,grid,size,desc) for i in range(batch_size)]
         [future.result() for future in futures]
     remove_dir(f"jobfiles/Run_{run_dir}")
 
 
-M1 = diagonal_matrix()
-M2 = stripe_matrix()
-M3 = block_diagonal_matrix()
-M4 = organized_alternating_matrix()
-M5 = random_matrix()
+# Configuration for the simulation or experiment
 
-M1_46 = diagonal_matrix_46()
-M2_46 = stripe_matrix_46()
-M3_46 = block_diagonal_matrix_46()
-M4_46 = organized_alternating_matrix_46()
-M5_46 = random_matrix(0.46)
+# False Positive Rate (P_fp) -> used in Environment::getSample(double x, double y)
+# Description: Define the probability percentage of false positives during sampling (only used for sample_strategy = 2)
+P_fp = 0 
 
+# False Negative Rate (P_fn) -> used in Environment::getSample(double x, double y)
+# Description: Define the probability percentage of false negatives during sampling (only used for sample_strategy = 2)
+P_fn = 0 
 
+# Sampling Strategy
+# Description: Define the sampling strategy to use; 0 = perfect sampling according to world file. 1 = using distributions, 2 = using FP and FN
+sample_strategy = 1  
 
+# Grid Start Seed
+# Description: Define the seed value for initializing the random number generator of the seed
+# This ensures that results can be reproduced across runs.
+grid_start_seed = 400  
 
-CAL_GRID = np.array([
-                    [0, 1, 0, 1, 0],  # Row corresponding to y=1.0 to y=0.8
-                    [1, 0, 0, 1, 0],  # Row corresponding to y=0.8 to y=0.6
-                    [1, 0, 1, 0, 1],  # Row corresponding to y=0.6 to y=0.4
-                    [0, 1, 1, 0, 1],  # Row corresponding to y=0.4 to y=0.2
-                    [1, 0, 0, 1, 0]   # Row corresponding to y=0.2 to y=0.0
-                ])
+# Full Run Flag
+# Description: Flag to indicate whether to run the full experiment or stop upon making a final decision
+run_full = 0  # Example: set to 0 for a partial run, 1 for a full run
 
-print(calculate_morans_I(CAL_GRID),entropy(CAL_GRID))
+# Batch Size
+# Description: Define the number of simulations to process in a single batch.
+# Example: 10 simulations per batch.
+batch_size = 10 
 
-P_fp = 0
-P_fn = 0
-sample_strategy = 1
-grid_start_seed = 400
-run_full=0
-batch_size = 100
-number_of_thread = 3
+# Number of Threads
+# Description: Define the number of threads to use for parallel processing.
+# Example: 3 threads for concurrent processing.
+number_of_thread = 3  # Example: set to 3 for three threads
 
+feedback = 1 #0 for no feedback, 1 positive feedback
 
-eta = 1500
-Usp = 2000
-x = [7860, 10725 , 3778  , 55,381]
+# Input Values of the robot state machine (I send them before)
+x = [7860, 10725, 3778, 55, 381]  
 
-
-
-# run_directory = 10 #Start dir for calibration us
-# grid_start_seed = 400
-# for eta in [750,1000,1250,1500,1750,2000,2250,2500]:
-#     for Usp in [1000,2000,3000,4000]:
-#         launch_batch(batch_size=batch_size,workers=number_of_thread,x_=x,run_dir=run_directory,feedback=2,fill_ratio=0.48,robots=5,size=5,desc=f"Calibration Us")   
-#         run_directory+=1
-
-# # """Code below runs the parallel launches for comparision of Umin, Uplus and Us over different fll-ratios"""
-
-# eta = 1500
-# Usp = 2000
-# run_directory = 50 #start dir
-# grid_start_seed = 400
-# for fill_ratio in [0.44,0.48,0.52,0.56]: #different grid sizes
-
-#     for feedback in [0,1,2]:#Umin, Uplus and Us
-#         launch_batch(batch_size=batch_size,workers=number_of_thread,x_=x,run_dir=run_directory,feedback=feedback,fill_ratio=fill_ratio,robots=5,size=5,desc=f"f{fill_ratio},feedbac strategy{feedback}")   
-#         run_directory+=1
-
-
-# # # """------------------------------------------------"""
-
-# run_directory = 100 # start dir
-# # # """Below code runs over multiple robots 2*6*3*100 = 3600 simulations"""
-# grid_start_seed = 400
-# for fill_ratio in [0.48]:
-#     for n_robots in [10,9,8,7,6,5]:
-#         for feedback in [0,1,2]:#Umin, Uplus and Us
-#             launch_batch(batch_size=batch_size,workers=1,x_=x,run_dir=run_directory,feedback=feedback,fill_ratio=fill_ratio,robots=n_robots,size=5,desc=f"f{fill_ratio},feedbac strategy{feedback}")   
-#             run_directory+=1
-
-
-
-batch_size=100
-x_empirical =  [7500,15000,2000,50,320]
-
-run_directory = 150 #Start dir
-grid_start_seed = 400
-for nrovs in [5,6,7,8,9,10]:
-    for pattern in [M1,M2,M3,M4,M5,M1_46,M2_46,M3_46,M4_46,M5_46]:
-        for pos in [x,x_empirical]:
-        
-            for feedback in [0,1,2]:#Umin, Uplus and Us
-                launch_batch(batch_size=batch_size,workers=1,x_=pos,run_dir=run_directory,feedback=feedback,fill_ratio=0.48,robots=nrovs,grid=pattern,size=pattern.shape[0],desc=f"Moran Index with n_robots {5}, feedback {feedback}",)   
-                run_directory+=1
-
-
+launch_batch(10, workers=number_of_thread, x_ = x, run_dir = 1, feedback = 0, fill_ratio=0.48, robots=5)
